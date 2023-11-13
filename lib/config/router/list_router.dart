8d29f254dee98aa38b630456026da1f4932a515d
GabrielEPadillaTEC <148036221+GabrielEPadillaTEC@users.noreply.github.com>
2023-11-13 00:16:28 -0800

    Push del Examen
    Examen de segunda para mi

---
+++ lib/config/router/list_router.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/screens/screens.dart';
+
+
+final List<RouteBase> routes = [
+  GoRoute(
+    path: '/',
+    name: HomeScreen.screenName,
+    builder: (BuildContext context, GoRouterState state) => const HomeScreen(),
+  ),
+  GoRoute(
+    path: '/buttons',
+    name: ButtonsScreen.screenName,
+    builder: (BuildContext context, GoRouterState state) => const ButtonsScreen(),
+  ),
+  GoRoute(
+    path: '/cards',
+    name: CardsScreen.screenName,
+    builder: (BuildContext context, GoRouterState state) => const CardsScreen(),
+  ),
+  GoRoute(
+    path: '/progress',
+    name: ProgressScreen.screenName,
+    builder: (BuildContext context, GoRouterState state) => const ProgressScreen(),
+  ),
+  GoRoute(
+    path: '/snackbars',
+    name: SnackBarScreen.screenName,
+    builder: (BuildContext context, GoRouterState state) => const SnackBarScreen(),
+  ),
+  GoRoute(
+    path: '/chat',
+    name: ChatScreen.screenName,
+    builder: (BuildContext context, GoRouterState state) => const ChatScreen(),
+  ),
+  GoRoute(
+    path: '/counter',
+    name: CounterScreen.screenName,
+    builder: (BuildContext context, GoRouterState state) => const CounterScreen(),
+  ),
+];
